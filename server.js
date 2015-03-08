a07e80b34b881ed50b65200d51e602dc07be7430
李伟 <hzliwei@corp.netease.com>
2015-03-08 12:24:20 +0800

    add doc

--- server.js
+++ server.js
@@ -8,4 +8,5 @@ server.listen(8000, '0.0.0.0', 511, function() {
     // // Once the server is listening we automatically open up a browser
     var open = require('open');
     open('http://localhost:' + 8000 + '/');
+    console.log('Server is startup!');
 });
